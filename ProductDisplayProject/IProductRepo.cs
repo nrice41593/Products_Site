7759be2e06df90ff83f49cb70abc1d6ba9d86a43
NathanRice <nrice@truecoders.io>
2020-10-05 16:45:38 -0500

    added base repo implementations

---
+++ ProductDisplayProject/IProductRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using ProductDisplayProject.Models;
+namespace ProductDisplayProject
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
---
+++ ProductDisplayProject/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProductDisplayProject.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
